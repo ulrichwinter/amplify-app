22c56ceab2f10493b5f32bc037c2a6cbb7d83324
Ulrich Winter <ulrich.winter1@senacor.com>
2020-08-06 12:04:31 +0200

    use api in app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { API } from 'aws-amplify'
 import { withAuthenticator, AmplifySignOut} from '@aws-amplify/ui-react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [coins, updateCoins] = useState([]);
+
+  async function fetchCoins() {
+    const data = await API.get('cryptoapi', '/coins')
+    updateCoins(data.coins);
+  }
+
+  useEffect(() => {
+    fetchCoins();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -23,6 +35,16 @@ function App() {
 
       <h1>Hello from AWS Amplify</h1>
       <AmplifySignOut />
+
+      <h2>Available Cryptocurrencies</h2>
+      {
+        coins.map((coin, index) => (
+        <div key={index}>
+          <h3>{coin.name} - {coin.symbol}</h3>
+          <h5>${coin.price_usd}</h5>
+        </div>
+        ))
+      }
     </div>
   );
 }
